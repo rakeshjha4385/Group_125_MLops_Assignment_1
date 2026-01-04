72e01b0186188206083f3e6023020a2015230fe7
Rakesh Jha <rakesh.jha4385@gmail.com>
2026-01-04 22:44:22 +0530

    Adding CI workflow and updating model artifacts

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import pandas as pd
 import mlflow.sklearn
 import logging
 
@@ -6,14 +7,32 @@ app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 
 MODEL_URI = "models:/Heart-Disease-MLOps/Production"
+
+# Load model once at startup
 model = mlflow.sklearn.load_model(MODEL_URI)
+
+# MUST match training features exactly
+FEATURE_COLUMNS = [
+    "age", "sex", "cp", "trestbps", "chol",
+    "fbs", "restecg", "thalach", "exang",
+    "oldpeak", "slope", "ca", "thal"
+]
 
 @app.post("/predict")
 def predict(features: dict):
     logging.info("Prediction request received")
-    X = [list(features.values())]
+
+    # #  THIS LINE FIXES EVERYTHING
+    # X = pd.DataFrame([features])
+
+    # # Enforce correct column order
+    # X = X[FEATURE_COLUMNS]
+
+    X = pd.DataFrame([features], columns=FEATURE_COLUMNS)
+
     pred = model.predict(X)[0]
     prob = model.predict_proba(X)[0][1]
+
     return {
         "prediction": int(pred),
         "confidence": float(prob)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,24 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import mlflow.sklearn
 
-def build_preprocessor(X):
-    numeric_features = X.columns
+NUM_FEATURES = [
+    "age", "sex", "cp", "trestbps", "chol",
+    "fbs", "restecg", "thalach", "exang",
+    "oldpeak", "slope", "ca", "thal"
+]
 
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("num", StandardScaler(), numeric_features)
-        ]
-    )
-    return preprocessor
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", StandardScaler(), NUM_FEATURES)
+    ]
+)
+
+pipeline = Pipeline(
+    steps=[
+        ("preprocessing", preprocessor),
+        ("model", RandomForestClassifier(random_state=42))
+    ]
+)
